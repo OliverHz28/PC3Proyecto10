4fbcb722b42e69d4b0acfba726c1f2725a36a05e
OliverHz28 <oliver_28z@hotmail.com>
2025-06-08 17:07:29 -0500

    feat(refactor): agregar incrementar_version en config_modifier.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from src.config_modifier import leer_json
+from src.config_modifier import leer_json, incrementar_version
 
 data = leer_json("config.json")
 print(data)
+
+numero_version = incrementar_version("config.json")
+print(f"Nueva versión: {numero_version}")
