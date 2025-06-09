3032b75efa1968e148f5e5d9099a81425e89ae84
GermainAN <germainchoquechambi5@gmail.com>
2025-06-09 00:16:51 -0500

    merge[#8]: feature/lint-hook a feature/develop (#10)
    * feat[#5]: agregar script para validar codigo Python con flake8
    
    * feat[#5]: añadir validacion de scripts bash con shellcheck
    
    * feat[#5]:: añadir validacion con tflint para IaC
    
    * feat[#8]: implementar hook para validar el formato del mensaje del commit
    
    * feat[#8]: implementar hook pre-push para prevenir push con errores

--- scripts/lint_all.sh
+++ scripts/lint_all.sh
@@ -1,1 +1,45 @@
 #!/usr/bin/env bash
+
+cd "$(dirname "$0")/.." || exit
+
+errores=0 
+
+echo "Ejecutando flake8"
+if flake8 src/ tests/ ./*.py --max-line-length=88 --select=E,W,F; then
+  echo "No se encontraron errores con flake8"
+else
+  echo "flake8 encontro errores"
+  errores=1
+fi
+
+echo "*********************"
+echo "Ejecutando shellcheck"
+if shellcheck scripts/* hooks/*; then 
+  echo "No se encontraron errores con shellcheck"
+else
+  echo "shellcheck encontro errores"
+  errores=1
+fi
+
+echo "*********************"
+echo "Ejecutando tflint"
+if [ -d "iac" ]; then
+  if tflint --enable-all iac/; then
+    echo "No se encontraron errores con tflint"
+  else
+    echo "tflint encontro errores"
+    errores=1
+  fi
+else
+  echo "No se encontro el directorio de IaC"
+fi
+
+echo "*********************"
+echo "Resultado Final:"
+if [ $errores -eq 1 ]; then
+  echo "Se encontraron errores"
+  exit 1
+else
+  echo "Todos los lint pasaron correctamente"
+  exit 0
+fi
