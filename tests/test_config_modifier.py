5864808521617bdde65155a8dcf318cdd8b6d439
Edy <166039525+EdySerrano@users.noreply.github.com>
2025-06-11 10:01:15 -0500

    merge[#2]: feature/tests  (#11)
    * test[#2]: agregando test json_de_prueba en test_config_modifier.py
    
    * test[#2]: agregando test test__leer_json_valido en test_config_modifier.py
    
    * test[#2]: agregando test test_incrementar_version en test_config_modifier.py
    
    * test[#2]: agregando test test_leer_json_invalido en test_config_modifier.py
    
    * test[#2]: agregando test test_incrementar_version_tipo_incorrecto en test_config_modifier.py
    
    * Actualizacion de test_config_modifier.py
    
    Ocurrio un problema con el pytest y windows pero ya se soluciono
    
    ---------
    
    Co-authored-by: edysaul20000 <edysaulserrano@gmail.com>

--- tests/test_config_modifier.py
+++ tests/test_config_modifier.py
@@ -2,8 +2,11 @@
 
 import pytest
 import json
+import sys
 import os
-from src.config_modifier import leer_json, incrementar_version
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+from config_modifier import leer_json, incrementar_version
 
 @pytest.fixture
 # Preparamos un archivo temporal para pruebas
