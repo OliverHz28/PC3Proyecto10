fee12c6b36b9d5637b34f88beecbf713ce90f27f
Germain <germainchoquechambi5@gmail.com>
2025-06-17 13:46:36 -0500

    test[#29]: agregar test para titulo de PR con formato incorrecto

--- tests/test_check_pr.py
+++ tests/test_check_pr.py
@@ -2,6 +2,7 @@ import os
 import tempfile
 from scripts.check_pr import validar_titulo
 
+
 # Verifica que el titulo de PR tiene el formato correcto
 def test_titulo_valido():
     with tempfile.TemporaryDirectory() as temp_dir:
@@ -15,4 +16,18 @@ def test_titulo_valido():
 
         ok, msg = validar_titulo(carpeta_pr)
         assert ok is True
-        assert msg == "OK"
+
+
+# verificar que el titulo de PR no tiene el formato correcto
+def test_titulo_mal_formato():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        pr_id = "124"
+        carpeta_pr = os.path.join(temp_dir, pr_id)
+        os.makedirs(carpeta_pr)
+        archivo = os.path.join(carpeta_pr, f"pr_{pr_id}_title.txt")
+
+        with open(archivo, "w", encoding="utf-8") as f:
+            f.write("PR que no tiene el formato correcto")  # Mal formato
+
+        ok, msg = validar_titulo(carpeta_pr)
+        assert ok is False
